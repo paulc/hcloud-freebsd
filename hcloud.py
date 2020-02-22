3d2ba49590821d5d65210871a8f03bd1e53c9d0a
PaulC <212168+paulc@users.noreply.github.com>
2020-02-22 14:20:34 +0000

    Update hcloud.py

--- hcloud.py
+++ hcloud.py
@@ -1,17 +1,18 @@
 #!/usr/local/bin/python3
 
+import email,json,pathlib,subprocess
 import requests
-import json
 import yaml
-import email
-import subprocess
 
 def sysrc(key,val=None):
 	if val:
 		subprocess.run(['/usr/sbin/sysrc','{}={}'.format(key,value)],check=True)
 	else:
 		subprocess.run(['/usr/sbin/sysrc',key],check=True)
-		
+
+def runrc(name):
+	subprocess.run(['/usr/sbin/service',name,'start'],check=True)
+	
 r = requests.get('http://169.254.169.254/hetzner/v1/metadata')
 
 if r.status_code != 200:
@@ -30,6 +31,37 @@ for k,v in config.items():
 					f.write(p.get_payload())
 					f.write("\n")
 	else:
-		with open(k,"w") as f:
+		# Save config section 
+		with open(k,'w') as f:
 			json.dump(config.get(k),f,indent=4)
 			f.write("\n")
+		# Handle section
+		if k == 'hostname':
+			sysrc('hostname',v)
+			runrc('hostname')
+		elif k == 'public-keys':
+			sshdir = pathlib('/root/.ssh')
+			sshdir.mkdir(mode=0o700,exist_ok=True)
+			ak = p / 'authorized_keys'
+			with ak.open('w') as f:
+				for sshkey in v:
+					f.write(sshkey)
+					f.write('\n')
+			ak.chmod(0o600)
+		elif k == 'network-config':
+			for iface in v['config']:
+				ifname = iface['name'].replace('eth','vtnet')
+				for net in iface['subnets']:
+					if net['ipv4']:
+						if type == 'dhcp':
+							# Dont configure IPv4 on vtnet0 - configure via DHCP
+							sysrc('ifconfig_{}'.format(ifname),'DHCP')
+						elif type == 'static':
+							pass
+					elif net['ipv6']:
+						if type == 'static':
+							address,prefix = net['address'].split('/')
+							sysrc('ifconfig_{}_ipv6'.format(ifname),'inet6 {} prefixlen {}'.format(address,prefix))
+						if 'gateway' in net:
+							sysrc('ipv6_defaultrouter',net['gateway'].replace('eth','vtnet'))
+			
